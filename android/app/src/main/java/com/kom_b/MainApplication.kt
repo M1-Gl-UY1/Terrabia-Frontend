725376a0add159eca6794e77f94e8607ce97d46f
Mambou213 <mambou.wallace2005@gmail.com>
2025-08-15 22:00:02 +0100

    V2

--- android/app/src/main/java/com/kom_b/MainApplication.kt
+++ android/app/src/main/java/com/kom_b/MainApplication.kt
@@ -1,5 +1,5 @@
 package com.kom_b
-
+import org.devio.rn.splashscreen.SplashScreen;
 import android.app.Application
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
