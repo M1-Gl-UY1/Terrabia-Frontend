104f053921d840bcf0cfa8528b58c35e6121e69b
Mambou213 <mambou.wallace2005@gmail.com>
2025-08-16 00:38:58 +0100

    V3

--- App.tsx
+++ App.tsx
@@ -5,13 +5,24 @@
  * @format
  */
 
+import React, { useEffect } from 'react';
 import { StatusBar, useColorScheme } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import SplashScreen from 'react-native-splash-screen'; // 📌 Import du module
 import AppNavigator from './src/navigation/AppNavigator';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
+
+  useEffect(() => {
+    // 📌 Masquer le splash screen natif après 2 secondes
+    const timer = setTimeout(() => {
+      SplashScreen.hide();
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <SafeAreaProvider>
